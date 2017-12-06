786ff400dd10c2e45067342a1af005f27e1ad8ed
Tomas Vojir <vojirtom@cmp.felk.cvut.cz>
2017-12-06 15:39:24 +0100

    generate plots to separate figures after the radar_plot update to work on plot axis instead of figures

--- radar_plot_examples.m
+++ radar_plot_examples.m
@@ -6,13 +6,16 @@ data_legend = {'data1', 'data2', 'data3'};
 dim_legend = {'X1', 'X2', 'X3', 'X4', 'X5'};
 params.axis_reverse = [1 0 0 1 0];
 params.data_symbols = {'o', 'x', 'o'};
+figure()
 fig1 = radar_plot(P, dim_legend, data_legend, params);
 
 params.circular_isocurves = true;
 params.dim_legend_font_size = 10;
+figure()
 fig2 = radar_plot(P, dim_legend, data_legend, params);
 
 params.isocurve_line_style = '--';
 params.show_dim_scale_label = false;
 params.rotate_dim_legend = false;
+figure()
 fig3 = radar_plot(P, dim_legend, data_legend, params);
